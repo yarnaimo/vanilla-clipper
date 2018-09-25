7ec45cdaa043ab0a22b94aa1b3cc0d6ccc072a0f
yamaimo <yarnaimo@gmail.com>
2018-09-25 14:53:14 +0900

    Exclude __tests__

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    testRegex: '((\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
