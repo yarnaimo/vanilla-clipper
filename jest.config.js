6d00ccebf0512e85d42498c95e6a0dc8c1cadce6
yamaimo <yarnaimo@gmail.com>
2018-09-30 20:48:42 +0900

    Add jest.setup.js

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.ts?(x)'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    setupTestFrameworkScriptFile: './jest.setup.js',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     testRegex: '((\\.|/)(test|spec))\\.(jsx?|tsx?)$',
