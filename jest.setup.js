fa66d60061413101d238f73ca9f1cacb36f0f490
yamaimo <yarnaimo@gmail.com>
2018-12-09 01:07:55 +0900

    🔧 Initialize configs

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,14 @@
 jest.setTimeout(10000)
+
+const micro = require('micro')
+const handler = require('serve-handler')
+
+const server = micro(async (req, res) => handler(req, res, { public: 'src/__tests__/public' }))
+
+beforeAll(() => {
+    return new Promise(resolve => server.listen(3000, resolve))
+})
+
+afterAll(() => {
+    server.close()
+})
